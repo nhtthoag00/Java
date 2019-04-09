afa044321b7a118539b483aa35d70ae276d995ae
nhtthoag00 <47780417+nhtthoag00@users.noreply.github.com>
2019-04-09 17:18:16 +0700

    add new file

---
+++ IntelliJ-Java/Days-In-Month/Month.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Month {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which month that you want to count days?");
+        int month = scanner.nextInt();
+
+        String daysInMonth;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                daysInMonth = "31";
+                break;
+            case 2:
+                daysInMonth = "28 or 29";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = "30";
+                break;
+            default:
+                daysInMonth = "";
+
+        }
+        if (daysInMonth != "") {
+            System.out.printf("The month %d have %s days.", month, daysInMonth);
+        } else {
+            System.out.print("Input invalid !!!!!");
+        }
+    }
+}
