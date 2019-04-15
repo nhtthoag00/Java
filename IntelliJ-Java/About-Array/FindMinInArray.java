d8cd4e52c3f95fba97d3c03e3b925fe0d5aaa26e
nhtthoag00 <47780417+nhtthoag00@users.noreply.github.com>
2019-04-15 15:49:33 +0700

    add new file

---
+++ IntelliJ-Java/About-Array/FindMinInArray.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexamples;
+
+/**
+ *
+ * @author Hoang Nhat
+ */
+public class FindMinInArray {
+    public static int minValue(int  []array){
+        int min = array[0];
+        int index = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in array is " + arr[index]);
+    }
+}
